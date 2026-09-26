b6c951fb4c8869f5bc7dfb794387274509d8082f
Faraz <ashraffaraaz6@gmail.com>
2026-09-26 12:54:40 +0530

    Time: 0 ms (100%), Space: 8.1 MB (6.19%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.cpp
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.cpp
@@ -12,18 +12,16 @@ public:
     int guessNumber(int n) {
         int low = 1;
         int high = n;
-        int mid = 0;
-        int guessedValue = 0;
 
         while(low <= high){
-            mid = low + (high - low) / 2;
-            guessedValue = guess(mid);
+           int mid = low + (high - low) / 2;
+           int  guessedValue = guess(mid);
 
             if(guessedValue == 0) return mid;
             else if(guessedValue == -1) high = mid - 1;
             else low = mid + 1;
         }
 
-        return guessedValue;
+        return -1;
     }
 };
